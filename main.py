2ca9fe5aced2d4daa37b2958831bcb2bc392acf5
Chen Xu <chenxu@Chens-iMac.local>
2019-12-10 19:01:02 +0800

    mod

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import datetime
 
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 
 @app.route('/')
